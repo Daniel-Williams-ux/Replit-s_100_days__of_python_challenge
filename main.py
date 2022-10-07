26ae2773c18337564bf5d6d88d090f9f40e1128a
Daniel Williams <williamsoligodaniel@gmail.com>
2022-10-07 05:41:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("""
+Name: Daniel
+
+DOB: March, 27 2022
+
+I am signing up for Replit's 100 days of Python challenge!
+
+I"ll be using Replit, an amazing online IDE so I can do this from phone wherever I happen to be. No excuses for not coding from the midle of a field!
+
+I am feeling 💪
+
+You can follow my progress on replit.com/DanielWilliams23
+""")
