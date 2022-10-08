3a6adcf13a37b31cf5f2eea9d73c422b2656f9d4
Daniel Williams <williamsoligodaniel@gmail.com>
2022-10-08 07:47:19 +0100

    Create concatenation.py

---
+++ concatenation.py
@@ -0,0 +1,7 @@
+myFood = input('Name a food: ')
+myPlant = input('Name a type of plant: ')
+myCookingMethod = input('What is your cooking method?: ')
+burnedFood = input('What word describes burned food?: ')
+myItem = input('Name a DIY item: ')
+
+print(myCookingMethod, myFood, 'with', burnedFood, myPlant, 'on a bed of', myItem)
