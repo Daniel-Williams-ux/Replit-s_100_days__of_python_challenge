c5e64c949bc63d83f63b2f972544d05a356ca9f7
Daniel Williams <williamsoligodaniel@gmail.com>
2022-10-12 09:01:35 +0100

    day 5

---
+++ if_else.py
@@ -0,0 +1,13 @@
+firstResponse = input('Do you like "hanging around"? ')
+if firstResponse == 'No':
+  print('Then you are not Spider-man')
+  print()
+
+secondResponse = input('Do you have a "gravelly" voice? ')
+if secondResponse == 'No':
+  print('Aww, then you are not Korg')
+  print()
+
+thirdResponse = input('Do you often fee "Marvelous"? ')
+if thirdResponse == 'yes':
+  print('Aha! You are Captain Marvel! Hi!')
