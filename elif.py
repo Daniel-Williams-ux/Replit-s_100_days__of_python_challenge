7b2efd1ab22394689497ea64c19055d9a490650f
Daniel Williams <williamsoligodaniel@gmail.com>
2022-10-13 08:11:50 +0100

    day 6

---
+++ elif.py
@@ -0,0 +1,10 @@
+if username == 'Daniel' and password == 'code':
+  print("Why hello there Daniel, what a lovely accent you have, you could have charmed your way in here even without a password")
+  print("Hello David")
+  print("Do not forget to wear a hat in the sun!")
+
+elif username == 'David' and password == 'totallyNotBad':
+  print("Hello David")
+
+else:
+  print("Go away")
