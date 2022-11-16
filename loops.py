780a3c84512935ce2100696d2b17b31ec2c229ee
Daniel Williams <williamsoligodaniel@gmail.com>
2022-11-16 06:26:43 +0100

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,16 @@
+counter = 0
+while counter < 10:
+  print(counter)
+  counter += 1
+  
+  
+  
+  exit = ""
+while exit = "yes":
+  print("🥳")
+exit = input("Exit?: ")
+
+'''
+Day 15 Challenge
+Write a program that loops. Inside the loop, ask the user what animal sound they want to hear.
+'''
