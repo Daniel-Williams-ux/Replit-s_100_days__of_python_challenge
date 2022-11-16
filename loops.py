411198bc03a038c062b5b86d27ccc868604a1fec
Daniel Williams <williamsoligodaniel@gmail.com>
2022-11-16 07:05:03 +0100

    day 15

--- loops.py
+++ loops.py
@@ -14,3 +14,28 @@ exit = input("Exit?: ")
 Day 15 Challenge
 Write a program that loops. Inside the loop, ask the user what animal sound they want to hear.
 '''
+animalType = input("What animal do you want? ")
+
+while animalType == "cow":  
+  animalType = 0
+  while animalType < 10:
+    print("moo")
+    animalType += 1
+
+while animalType == "dog":  
+  animalType = 0
+  while animalType < 10:
+    print("woo woo")
+    animalType += 1
+
+while animalType == "lion":  
+  animalType = 0
+  while animalType < 10:
+    print("roar")
+    animalType += 1    
+  
+exit = input("do you want to exit? ")
+if exit == "yes":
+  print("Game over")
+else:
+  print("Start a new game")
