a2f28f46e636756b42157594818401a81a21aad9
Daniel Williams <williamsoligodaniel@gmail.com>
2022-12-14 10:52:50 +0100

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,21 @@
+'''
+For Loop
+A while loop is perfect to use when we don't know how many times we want the loop to repeat.
+
+If we have an idea of how many times we want the loop to repeat, we can use a for loop to loop code in exactly the same way the while loop did.
+
+However, we can set up the variable, control condition, and increment all in ONE line of code.
+
+Let's compare
+Here is how we created a counter with a while loop:
+'''
+counter = 0
+while counter < 10:
+  print(counter)
+  counter += 1
+  
+'''
+And here is the same counter using a for loop:
+'''
+for counter in range(10):
+  print(counter)
