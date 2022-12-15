22e197d2ab0519609a7534bd7df84d29487485ad
Daniel Williams <williamsoligodaniel@gmail.com>
2022-12-15 07:10:23 +0100

    Update for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -19,3 +19,15 @@ And here is the same counter using a for loop:
 '''
 for counter in range(10):
   print(counter)
+
+'''
+Range
+The range function creates a list of numbers in the range you create. If you only give it one number, 
+it will start at 0 and move to a state where the final number is one less than the number in the brackets. In this case, the final number would be 9.
+
+range(10)
+Note: The variable is only there during the loop, not after it is completed.
+
+Fun fact
+Commonly computer programmers use the variable names i, j, and k when using for loops for counter. 
+There is no real reason. It's just how everyone has always done it. However, feel free to use a variable that has a bit more meaning if you like.
